73d5c5267be28e4983517c7bb7135e50968a67b6
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2019-01-31 22:39:18 +0100

    commenting

--- code.js
+++ code.js
@@ -10,9 +10,9 @@
     var skills_html = "";
 
     for (var skill in skills) { 
-        if (skills[skill] > 0) { // skip 0 points
+        if (skills[skill] > 0) { // skip skills with 0 points
             
-            if (skill != "total") { // skip "total"
+            if (skill != "total") { // skip "total" skill
                 skills_html += 
                     "<li><p>" +
                         skill + ": " + skills[skill] + 
@@ -20,8 +20,8 @@
             }
         }
     }
-    
-    profile_skills.innerHTML = skills_html;
+
+    profile_skills.innerHTML = skills_html; // render results
 
     // render badges
     const profile_badges = document.getElementById("badges");
@@ -29,7 +29,9 @@
     var badges_html = "";
 
     for (var badge in badges) {
-        if (badges[badge].name != "Newbie") { // skip newbie badge
+        if (badges[badge].name != "Newbie") { // skip "newbie" badge
+            
+            // get courses within badge
             var course_list = "";
             for (var course in badges[badge].courses) {
                 course_list += badges[badge].courses[course].title + ", ";
@@ -47,7 +49,7 @@
         }                
     }
 
-    profile_badges.innerHTML = badges_html;
+    profile_badges.innerHTML = badges_html; // render results
 
     // scrollreveal.js
     ScrollReveal().reveal('.badge');
